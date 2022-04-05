1ec7ed0e3af9c432685e3081bad0eeda610953de
karkadi <karkadi@gmail.com>
2022-04-06 00:22:18 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,8 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+  name: "Cucumberish",
+  platforms: [.iOS("7.0"), .macOS("10.9"), .tvOS("9.0")],
+  products: [.library(name: "Cucumberish", targets: ["Cucumberish"])],
+  targets: [.target(name: "Cucumberish", path: "Cucumberish")])
