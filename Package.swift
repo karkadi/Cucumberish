a1c34506f3b8d3103d41ec2762a81803815232e5
karkadi <karkadi@gmail.com>
2022-04-06 00:28:45 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,5 +4,9 @@ import PackageDescription
 let package = Package(
   name: "Cucumberish",
   platforms: [.iOS("7.0"), .macOS("10.9"), .tvOS("9.0")],
+     dependencies: [
+      //Remote package URL
+        .package(name: "PackageName2", url: "https://github.com/karkadi/Cucumberish", .branch("master"))
+    ]
   products: [.library(name: "Cucumberish", targets: ["Cucumberish"])],
   targets: [.target(name: "Cucumberish", path: "Cucumberish")])
