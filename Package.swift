18402fdd04efea6216d43cd5cc4344697ced6fc7
karkadi <karkadi@gmail.com>
2022-04-06 17:17:22 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,8 +1,18 @@
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
   name: "Cucumberish",
-  platforms: [.iOS("7.0"), .macOS("10.9"), .tvOS("9.0")],
-     dependencies: [
-        .Package(name: "Cucumberish", url: "https://github.com/karkadi/Cucumberish", .branch("master"))
-    ])
+  platforms: [.iOS("7.0"),
+    .macOS("10.9"),
+    .tvOS("9.0")],
+  products: [.library(name: "Cucumberish",
+                      targets: ["Cucumberish"])],
+  dependencies: [],
+  targets: [.target(name: "Cucumberish",
+                    path: "Cucumberish"),
+            .testTarget(
+              name: "CucumberishTests",
+              dependencies: ["Cucumberish"],
+              path: "CucumberishLibraryTest"),
+           ])
