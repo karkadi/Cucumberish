c018dd4920937f0e27797216c288c15d75b2f8ca
karkadi <karkadi@gmail.com>
2022-04-06 19:21:53 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,18 +3,26 @@ import PackageDescription
 
 let package = Package(
     name: "Cucumberish",
-    platforms: [.iOS("7.0"),
+    platforms: [.iOS("10.0"),
                 .macOS("10.9"),
                 .tvOS("9.0")],
     products: [.library(name: "Cucumberish",
+                        type: .static,
                         targets: ["Cucumberish"])],
     dependencies: [],
     targets: [.target(name: "Cucumberish",
                       path: "Cucumberish",
-                      exclude: ["../.git/"] ),
-              .testTarget(
-                name: "CucumberishLibraryTest",
-                dependencies: ["Cucumberish"],
-                path: "CucumberishLibraryTest",
-                exclude: ["../.git/"])]
+                      // exclude: ["../.git/"],
+                     sources: ["Cucumberish.m"
+ //"Dependencies/Gherkin/GHHasLocationProtocol.m"
+                               ],
+        //              publicHeadersPath: ".",
+                      cSettings: [
+                        .headerSearchPath("Core"),
+                        .headerSearchPath("Core/Managers"),
+                        .headerSearchPath("Core/Models"),
+                        .headerSearchPath("Utils"),
+                        .headerSearchPath("Dependencies/Gherkin"),
+                      ]
+                     )]
 )
