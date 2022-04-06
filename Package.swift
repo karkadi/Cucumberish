b160a285050610362a27747fc69c37b05ac62372
karkadi <karkadi@gmail.com>
2022-04-06 17:44:27 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,17 +2,19 @@
 import PackageDescription
 
 let package = Package(
-  name: "Cucumberish",
-  platforms: [.iOS("7.0"),
-    .macOS("10.9"),
-    .tvOS("9.0")],
-  products: [.library(name: "Cucumberish",
-                      targets: ["Cucumberish"])],
-  dependencies: [],
-  targets: [.target(name: "Cucumberish",
-                    path: "Cucumberish"),
-            .testTarget(
-              name: "CucumberishTests",
-              dependencies: ["Cucumberish"],
-              path: "CucumberishLibraryTest"),
-           ])
+    name: "Cucumberish",
+    platforms: [.iOS("7.0"),
+                .macOS("10.9"),
+                .tvOS("9.0")],
+    products: [.library(name: "Cucumberish",
+                        targets: ["Cucumberish"])],
+    dependencies: [],
+    targets: [.target(name: "Cucumberish",
+                      path: "Cucumberish",
+                      exclude: ["../.git/"] ),
+              .testTarget(
+                name: "CucumberishLibraryTest",
+                dependencies: ["Cucumberish"],
+                path: "CucumberishLibraryTest",
+                exclude: ["../.git/"])]
+)
